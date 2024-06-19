c49f6dabfa12e9806217691f9d88670a04482f87
Siva Gollapalli <sivagollapalli88@gmail.com>
2024-06-19 15:05:01 +0530

    Restructed the app

--- assetapp/apps.py
+++ assetapp/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class AssetConfig(AppConfig):
+class AssetappConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
-    name = "asset"
+    name = "assetapp"
