29aa91846d59aed468772c8be44e97dda9533535
Siva Gollapalli <sivagollapalli88@gmail.com>
2024-06-19 15:13:38 +0530

    Added initial model asset

--- assetapp/models.py
+++ assetapp/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Asset(models.Model):
+  file_name = models.CharField(max_length=255, blank=True)
+  document = models.FileField(upload_to='uploads/')
+  uploaded_at = models.DateTimeField(auto_now_add=True)
+  updated_at = models.DateTimeField(auto_now_add=True)
